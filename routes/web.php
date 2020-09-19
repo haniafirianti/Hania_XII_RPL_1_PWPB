4b7671f39bca5c497aa9dd4305de88e201b4b888
haniafirianti <haniafirianti@gmail.com>
2020-09-19 12:21:58 +0700

    [W] Membuat Login dan Register Multi Akses Menggunakan Spatie

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('role:admin')->get('/dashboard', function(){
+	return 'Dasboard';
+})->name('dashboard');
+
+

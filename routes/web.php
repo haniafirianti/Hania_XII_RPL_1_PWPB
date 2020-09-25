e0e2ee1dcbcade956cdc35f4f453d01269b466d2
haniafirianti <haniafirianti@gmail.com>
2020-09-25 16:12:22 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::middleware('role:admin')->get('/dashboard', function(){
-	return 'Dasboard';
+	return 'Ini Dashboard';
 })->name('dashboard');
 
 
